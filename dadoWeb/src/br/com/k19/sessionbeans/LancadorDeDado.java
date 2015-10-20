da952ed7431fec01e53ec188c2d4f74dde22763b
Sheyster <eltonlferreira@gmail.com>
2015-10-20 19:05:56 -0200

    testando @Remote

---
+++ dadoWeb/src/br/com/k19/sessionbeans/LancadorDeDado.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package br.com.k19.sessionbeans;
+
+/**
+ * @author TON
+ *
+ */
+public interface LancadorDeDado {
+
+	int lanca();
+}
--- dadoWeb/src/br/com/k19/sessionbeans/LancadorDeDadoBean.java
+++ dadoWeb/src/br/com/k19/sessionbeans/LancadorDeDadoBean.java
@@ -2,13 +2,27 @@ package br.com.k19.sessionbeans;
 
 import java.util.Random;
 
+import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
 @Stateless
-public class LancadorDeDadoBean {
+@Remote(LancadorDeDado.class)
+public class LancadorDeDadoBean implements LancadorDeDado {
+
 	private Random gerador = new Random();
 	
-	public int lanca(){
+	@Override
+	public int lanca() {
 		return this.gerador.nextInt(6) + 1;
 	}
+	
 }
+// Este codigo mostra o funcionanto de um EBJ 3.1 onde não temos a anotação de @Local e nem a interface
+//@Stateless
+//public class LancadorDeDadoBean {
+//	private Random gerador = new Random();
+//	
+//	public int lanca(){
+//		return this.gerador.nextInt(6) + 1;
+//	}
+//}
