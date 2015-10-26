f1edfc02828f16511dff825865f735724af137b3
Sheyster <eltonlferreira@gmail.com>
2015-10-26 20:08:40 -0200

    Não está funcionando ainda, melhorar mostrar somente os repetidos

--- ManipulacaoString/src/br/com/manipulacaostring/ManipulacaoString.java
+++ ManipulacaoString/src/br/com/manipulacaostring/ManipulacaoString.java
@@ -70,5 +70,72 @@ public class ManipulacaoString {
 				System.out.println(testando[i]);
 			}
 		}
+		
+		
+		String caracterRepetido = "caracter repetido";
+		
+		char[] caracter = caracterRepetido.toCharArray();
+		
+		for (int i = 0; i < caracter.length; i++) {
+			char x = caracter[i];
+			for (int j = i + 1; j < caracter.length; j++) {
+				char y = caracter[j];
+				if(x == y){
+					System.out.println("Caracter repetido: " + x);
+				}
+			}
+		}
+		teste();
+		
+//		String caracterRepetido2 = "caracter repetido";
+//		
+//		char[] caracter2 = caracterRepetido2.toCharArray();
+//		char[] aux = null;
+//		
+//		for (int i = 0; i < caracter2.length; i++) {
+//			char x = caracter2[i];
+//			for (int j = i + 1; j < caracter2.length; j++) {
+//				char y = caracter2[j];
+//				if(x == y){
+//					if(aux != null){
+//						for (int k = 0; k < aux.length; k++) {
+//							if(x != aux[k]){
+//								
+//								System.out.println("Caracter repetido: " + x);
+//							}
+//						}
+//					} else {
+//						aux[0] = x;
+//					}
+//				}
+//			}
+//		}
+		
+	}
+	
+	public static void teste(){
+		String caracterRepetido2 = "caracter repetido";
+		
+		String aux = "";
+		
+		for (int i = 0; i < caracterRepetido2.length(); i++) {
+			char x = caracterRepetido2.charAt(i);
+			for (int j = i + 1; j < caracterRepetido2.length(); j++) {
+				char y = caracterRepetido2.charAt(j);
+				if(x == y){
+					if(!aux.isEmpty()){
+						for (int k = 0; k < aux.length(); k++) {
+							if(x != aux.charAt(k)){
+								aux += x;
+								System.out.println("Caracter repetido: " + x);
+								break;
+							}
+						}
+					} else {
+						aux += x;
+					}
+				}
+			}
+		}
 	}
 }
