dad1269dfdf3ccef33c4101d89886fd0ff1b2c4e
Sheyster <eltonlferreira@gmail.com>
2015-11-29 11:52:41 -0200

    Terminando alua 3

--- Java_III/src/br/com/alura/programa/TestaString.java
+++ Java_III/src/br/com/alura/programa/TestaString.java
@@ -19,10 +19,36 @@ public class TestaString {
 		System.out.println(s);
 
 		if (s.contains(s3)) {
-			System.out.println("A string " + s3 + " contém detro da string " + s);
+			System.out.println("A string " + s3 + " contém detro da string "
+					+ s);
 		} else {
-			System.out.println("A string " + s3 + " não contém detro da string " + s);
+			System.out.println("A string " + s3
+					+ " não contém detro da string " + s);
 		}
+
+		String test = "12";
+		char[] num = new char[test.length()];
+		int x = 0;
+
+		for (int i = 0; i < test.length(); i++) {
+			num[i] = test.charAt(i);
+			x = Character.getNumericValue(num[i]);
+			System.out.print(x);
+		}
+
+		System.out.println();
+		int result = converteParaInt(test);
+		System.out.println(result);
 	}
 
+	// Método Alura
+	private static int converteParaInt(String numero) {
+		int resultado = 0;
+		while (numero.length() > 0) {
+			char algarismo = numero.charAt(0);
+			resultado = resultado * 10 + (algarismo - '0');
+			numero = numero.substring(1);
+		}
+		return resultado;
+	}
 }
