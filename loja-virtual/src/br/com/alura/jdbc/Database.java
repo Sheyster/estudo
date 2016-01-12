109cacf6446f2c71701229b77289a9e5035fd3e4
Elton Ferreira <eltonlferreira@gmail.com>
2016-01-12 08:52:02 -0200

    continuar

--- loja-virtual/src/br/com/alura/jdbc/Database.java
+++ loja-virtual/src/br/com/alura/jdbc/Database.java
@@ -7,8 +7,7 @@ import java.sql.SQLException;
 public class Database {
 
 	public static Connection getConnection() throws SQLException {
-		return DriverManager.getConnection(
-				"jdbc:hsqldb:hsql://localhost/loja-virtual", "sa", "");
+		return DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/loja-virtual", "SA", "");
 	}
 
 }
--- loja-virtual/src/br/com/alura/jdbc/TestaInsercao.java
+++ loja-virtual/src/br/com/alura/jdbc/TestaInsercao.java
@@ -1,6 +1,7 @@
 package br.com.alura.jdbc;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -9,16 +10,16 @@ public class TestaInsercao {
 
 	public static void main(String[] args) throws SQLException {
 		Connection connection = Database.getConnection();
-		Statement statement = connection.createStatement();
+		String nome = "Notebook";
+		String descrição = "Dell´s";
 
-		boolean resultado = statement
-				.execute(
-						"insert into produto (nome, descricao) values ('Notebook', 'Notebook i5')",
-						Statement.RETURN_GENERATED_KEYS);
+		String sql;
+		sql = "insert into produto (nome, descricao) values (?, ?)";
+		PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+		statement.setString(0, nome);
+		statement.setString(1, descrição);
 
 		ResultSet resultSet = statement.getGeneratedKeys();
-
-		System.out.println("O resultado foi: " + resultado);
 
 		while (resultSet.next()) {
 			System.out.println(resultSet.getString("id"));
