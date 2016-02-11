21c42ff86105a562d2a721cd80b71bf642f28db0
Elton Ferreira <eltonlferreira@gmail.com>
2016-02-11 08:54:47 -0200

    Termino 1. Menos erros, mais produtividade

--- fj15-empresa/src/main/java/br/com/caelum/empresa/Principal.java
+++ fj15-empresa/src/main/java/br/com/caelum/empresa/Principal.java
@@ -1,5 +1,19 @@
 package br.com.caelum.empresa;
 
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+import br.com.caelum.empresa.modelo.Funcionario;
+
 public class Principal {
-	
+	public static void main(String[] args) {
+		Calendar dataNascimento = new GregorianCalendar(1984, 4, 28);
+		Funcionario funcionario = new Funcionario("José", 25, dataNascimento);
+		System.out.println(funcionario);
+	}
+	// public static void main(String[] args) {
+	// Calendar dataNascimento = new GregorianCalendar(1984, 4, 28);
+	// Funcionario funcionario = new Funcionario("José", 25, dataNascimento);
+	// System.out.println(funcionario);
+	// }
 }
