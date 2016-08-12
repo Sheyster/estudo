24952dbf0f80aa080c1fdbd72cf69ca50d2311bb
Elton Ferreira <eltonlferreira@gmail.com>
2016-08-12 08:37:07 -0300

    termino da parte de java do site hackerrank

---
+++ CursoAluraAlgortimo/src/br/com/teste/hackerrank/introdution/SolutionIfElse.java
@@ -0,0 +1,39 @@
+package br.com.teste.hackerrank.introdution;
+
+import java.util.Scanner;
+
+public class SolutionIfElse {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		String ans = "";
+		// if (n % 2 == 1) {
+		// ans = "Weird";
+		// } else {
+		// if (n >= 2 && n <= 5) {
+		// ans = "Not Weird";
+		// } else {
+		// if (n >= 6 && n <= 20) {
+		// ans = "Weird";
+		// } else {
+		// if (n > 20) {
+		// ans = "Not Weird";
+		// }
+		// }
+		// }
+		// }
+
+		if (n % 2 == 1) {
+			ans = "Weird";
+		} else if (n >= 2 && n <= 5) {
+			ans = "Not Weird";
+		} else if (n >= 6 && n <= 20) {
+			ans = "Weird";
+		} else if (n > 20) {
+			ans = "Not Weird";
+		}
+		System.out.println(ans);
+
+	}
+}
