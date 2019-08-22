9a46decb9f2a21c1d733c5a28d2b70e77dc6f857
Elton Ferreira <eltonlferreira@gmail.com>
2019-08-22 09:47:13 -0300

    Classe utilizada em alguns teste para responder aos questionarios Teste OO

---
+++ OOMelhoresTecnicas/src/br/com/alura/ooMelhoresTecnicas/teste/test.java
@@ -0,0 +1,34 @@
+package br.com.alura.ooMelhoresTecnicas.teste;
+
+public class test {
+	public static void main(String args[]) {
+//		Integer intObj = Integer.valueOf(args[args.length - 1]);
+//		int i = intObj.intValue();
+
+//		if (args.length > 1)
+//			System.out.println(i);
+//		if (args.length > 0)
+//			System.out.println(i - 1);
+//		else
+//			System.out.println(i - 2);
+
+//		for (int h = 1; h < 3; h++)
+//			for (int j = 3; j > h; j--)
+//				assert h != j;
+//		{
+//			System.out.println(h);
+//		}
+
+//		String test = "abc";
+//		test = test + test;
+//		System.out.println(test);
+//		
+//		char c;
+//		int i;
+//		c = 'A';		// 1
+//		i = c;		//2
+//		c = i + 1;	//3
+//		c++;		//4
+
+	}
+}
