89752874d52a17a25653bc511dcbfe4de1419993
Elton Ferreira <eltonlferreira@gmail.com>
2019-09-09 14:57:14 -0300

    JavaScript avançado I, Aula 6 falta coisa ainda kkk Video 4 Final

--- JavaScritpAvancadoI/WebContent/js/app/views/MensagemView.js
+++ JavaScritpAvancadoI/WebContent/js/app/views/MensagemView.js
@@ -1,25 +1,10 @@
-<<<<<<< HEAD
 class MensagemView extends View{
 
 	constructor(elemento) {
 		super(elemento);
 	}
 	
-	_template(model) {
+	template(model) {
 		return model.texto ? `<p class="alert alert-info"> ${model.texto} </p>` : "<p></p>";
-=======
-class MensagemView {
-
-	constructor(elemento) {
-		this._elemento = elemento;
-	}
-	
-	_template(model) {
-		return model.texto ? `<p class="alert alert-info"> ${model.texto} </p>` : "<p></p>";
-	}
-	
-	update(model) {
-		this._elemento.innerHTML = this._template(model);
->>>>>>> branch 'master' of https://github.com/Sheyster/estudo.git
 	}
 }
