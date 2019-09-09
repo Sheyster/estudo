29657202771000fbf8524d4e06ba5cfe44247540
Elton Ferreira <eltonlferreira@gmail.com>
2019-09-09 10:30:21 -0300

    JavaScript avançado I, Aula 4 falta coisa ainda kkk

--- JavaScritpAvancadoI/WebContent/js/app/helpers/DateHelper.js
+++ JavaScritpAvancadoI/WebContent/js/app/helpers/DateHelper.js
@@ -5,10 +5,16 @@ class DateHelper {
 	}
 	
 	static dataParaTexto(data) {
-		return data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear();
+		return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
+		// outra forma de escrever o mesmo código abaixo! Utilizando "template string"
+// return data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear();
 	}
 	
 	static textoParaData(texto) {
+		// Expressão regular
+		if(!/\d{4}-\d{2}-\d{2}/.test(texto)){
+			throw new Error("A data deve estar no formado aaaa-mm-dd!");
+		}
 		return new Date(...texto.split("-").map((item, indice) => item - indice % 2));
 	}
 }
