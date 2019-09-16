19a69b6da9dce2291390d3a51dbec2a965fe636c
Elton Ferreira <eltonlferreira@gmail.com>
2019-09-16 14:06:18 -0300

    JavaScript avançado II, Aula 2, Video 9 Final

--- JavaScritpAvancadoII/WebContent/js/app/controllers/NegociacaoController.js
+++ JavaScritpAvancadoII/WebContent/js/app/controllers/NegociacaoController.js
@@ -8,8 +8,26 @@ class NegociacaoController{
 		this._inputQuantidade = $("#quantidade");
 		this._inputValor = $("#valor");
 
-		//??????
-		this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
+		// ??????
+		// this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
+		let self = this;
+		this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+    		
+    		get(target, prop, receiver) {
+    			if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)){
+    				return function() {
+    					console.log(`interceptando ${prop}`);
+    					Reflect.apply(target[prop], target, arguments);
+    					self._negociacoesView.update(target);
+    				}
+    			}
+    			
+    			return Reflect.get(target, prop, receiver);
+    			/*
+				 * console.log(`Valor antigo: ${target[prop]} - Novo valor: ${value}`); return Reflect.set(target, prop, value, receiver);
+				 */
+    		}
+    	});
 		
 		this._negociacoesView = new NegociacoesView($("#negociacoesView"));
 		this._negociacoesView.update(this._listaNegociacoes);
