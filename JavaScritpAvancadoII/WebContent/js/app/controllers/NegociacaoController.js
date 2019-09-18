086adf1bf7789aa8738086412a5ea79f03534fc7
Elton Ferreira <eltonlferreira@gmail.com>
2019-09-18 10:47:56 -0300

    JavaScript avançado II, Aula 3, Video 1

--- JavaScritpAvancadoII/WebContent/js/app/controllers/NegociacaoController.js
+++ JavaScritpAvancadoII/WebContent/js/app/controllers/NegociacaoController.js
@@ -10,30 +10,13 @@ class NegociacaoController{
 
 		// ??????
 		// this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
-		let self = this;
-		this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
-    		
-    		get(target, prop, receiver) {
-    			if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop]) == typeof(Function)){
-    				return function() {
-    					console.log(`interceptando ${prop}`);
-    					Reflect.apply(target[prop], target, arguments);
-    					self._negociacoesView.update(target);
-    				}
-    			}
-    			
-    			return Reflect.get(target, prop, receiver);
-    			/*
-				 * console.log(`Valor antigo: ${target[prop]} - Novo valor: ${value}`); return Reflect.set(target, prop, value, receiver);
-				 */
-    		}
-    	});
+		this._listaNegociacoes = ProxyFactory.create(new ListaNegociacoes(), ["adiciona", "esvazia"], model => this._negociacoesView.update(model));
 		
 		this._negociacoesView = new NegociacoesView($("#negociacoesView"));
 		this._negociacoesView.update(this._listaNegociacoes);
 		// Object.freeze(this); // Essa funcao faz com não consigamos alterar o valor de um atributo de uma classe, similar ao private do Java
 		
-		this._mensagem = new Mensagem();
+		this._mensagem = ProxyFactory.create(new Mensagem(), ["texto"], (model) => this._mensagemView.update(model));
 		this._mensagemView = new MensagemView($("#mensagemView"));
 		this._mensagemView.update(this._mensagem);
 	}
@@ -44,18 +27,13 @@ class NegociacaoController{
 				// para um arrow function que contenha somente uma linha de decisão podemos deixar ela menos verbosa da sequinte forma
 		let data2 = new Date(...this._inputData.value.split("-").map((item, indice) => item - indice % 2));
 		this._listaNegociacoes.adiciona(this._criaNegociacao());
-		
 		this._mensagem.texto = "Negociação adicionada com sucesso!";
-		this._mensagemView.update(this._mensagem);
-		
 		this._limpaFormulario();
 	}
 	
 	apaga() {
 		this._listaNegociacoes.esvazia();
-		
 		this._mensagem.texto = "Negociações apagadas com sucesso!";
-		this._mensagemView.update(this._mensagem);
 	}
 	
 	_criaNegociacao(){
