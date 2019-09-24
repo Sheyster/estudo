662a9048df9434f403e6abb9a2835d7da68f3137
Elton Ferreira <eltonlferreira@gmail.com>
2019-09-24 08:23:16 -0300

    JavaScript avançado II, Aula 4, Video 2

--- JavaScritpAvancadoII/WebContent/client/js/app/controllers/NegociacaoController.js
+++ JavaScritpAvancadoII/WebContent/client/js/app/controllers/NegociacaoController.js
@@ -23,6 +23,35 @@ class NegociacaoController{
 		this._limpaFormulario();
 	}
 	
+	importaNegociacoes(){
+		let xhr = new XMLHttpRequest();
+		xhr.open("GET", "negociacoes/semana");
+		
+		xhr.onreadystatechange = () => {
+			/*
+			 * 0: requisição ainda não iniciada
+			 * 
+			 * 1: conexão com o servidor estabelecidad
+			 * 
+			 * 2: requisição recebida
+			 * 
+			 * 3: processando requisição
+			 * 
+			 * 4: requisição concluida e a resposta esta pronta
+			 */
+			if(xhr.readyState == 4){
+				if(xhr.status == 200){
+					console.log("Obtendo as negociações do servidor.");
+				} else {
+					console.log("Não foi possível obter as negociações do servidor.");
+				}
+			}
+			
+		};
+		
+		xhr.send();
+	}
+	
 	apaga() {
 		this._listaNegociacoes.esvazia();
 		this._mensagem.texto = "Negociações apagadas com sucesso!";
