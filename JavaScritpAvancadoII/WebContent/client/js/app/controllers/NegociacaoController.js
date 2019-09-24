c028a83e1b2b7e6b80e430fb01cf4b57ebf73574
Elton Ferreira <eltonlferreira@gmail.com>
2019-09-24 08:23:16 -0300

    JavaScript avançado II, Aula 4, Video 3

--- JavaScritpAvancadoII/WebContent/client/js/app/controllers/NegociacaoController.js
+++ JavaScritpAvancadoII/WebContent/client/js/app/controllers/NegociacaoController.js
@@ -23,6 +23,41 @@ class NegociacaoController{
 		this._limpaFormulario();
 	}
 	
+	importaNegociacoes(){
+		let xhr = new XMLHttpRequest();
+		xhr.open("GET", "negociacoes/semana");
+		
+		xhr.onreadystatechange = () => {
+			/*
+			 * 0: requisição ainda não iniciada
+			 * 
+			 * 1: conexão com o servidor estabelecidad
+			 * 
+			 * 2: requisição recebida
+			 * 
+			 * 3: processando requisição
+			 * 
+			 * 4: requisição concluida e a resposta esta pronta
+			 */
+			if(xhr.readyState == 4){
+				if(xhr.status == 200){
+					console.log("Obtendo as negociações do servidor.");
+					console.log(JSON.parse(xhr.responseText));
+					JSON.parse(xhr.responseText).map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+					.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+					this._mensagem.texto = "Negociações encontradas com sucesso!"
+				} else {
+					console.log("Não foi possível obter as negociações do servidor.");
+					console.log(xhr.responseText);
+					this._mensagem.texto = "Não foi possível obter as negociações."
+				}
+			}
+			
+		};
+		
+		xhr.send();
+	}
+	
 	apaga() {
 		this._listaNegociacoes.esvazia();
 		this._mensagem.texto = "Negociações apagadas com sucesso!";
